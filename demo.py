7a8d699a4b855bc6053e414403951eaba099d351
tomaszschwiertz <Mars123$Mars123$>
2020-09-19 16:34:54 +0100

    it polls from git

--- demo.py
+++ demo.py
@@ -1,2 +1,21 @@
-print("Hello")
-print ("TOM")
+import requests
+
+url="https://raw.githubusercontent.com/tomaszschwiertz/lines/master/prefix"
+read_data = requests.get(url).content.decode('utf8')
+
+print(read_data)
+
+
+list = read_data.split()
+str3 = """
+"""
+
+
+#str="set " + "set ".join(str(i) + "\n" for i in list)
+
+
+for x in range (0, len(list)):
+    str3+=("set "+list[x] + "\n")
+
+#print (list)
+print(str3)
